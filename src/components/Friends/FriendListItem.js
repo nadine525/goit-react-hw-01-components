beed3817e4ea55a3f033262cc3e2af0233ba46a7
nadine525 <02_seeders_wide@icloud.com>
2023-06-02 09:11:08 +0200

    props id

--- src/components/Friends/FriendListItem.js
+++ src/components/Friends/FriendListItem.js
@@ -12,8 +12,7 @@ export function FriendListItem({ avatar, name, isOnline }) {
 }
 
 FriendListItem.propTypes = {
-  id: PropTypes.number,
-  avatar: PropTypes.string,
-  name: PropTypes.string,
-  isOnline: PropTypes.bool,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
